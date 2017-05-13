27f77bbdd93ac9897fb3a42d964617a3ef4e06c0
Nick Reuter <nreuter85@gmail.com>
2017-05-13 01:17:21 -0500

    widevine: cleanup copying libs
    Change-Id: Ie51c8eaf947891d49a4c9d13fc8b250ca3827476

--- arm-generic/widevine-vendor.mk
+++ arm-generic/widevine-vendor.mk
@@ -1,10 +1,8 @@
 VENDOR_WV_PATH := vendor/widevine/arm-generic
 
-$(call inherit-product-if-exists, $(VENDOR_WV_PATH)/widevine-vendor-blobs.mk)
-
-PRODUCT_PACKAGES += \
-	libdrmwvmplugin \
-	libwvdrmengine \
-	libwvdrm_L3 \
-	libwvm \
-	libWVStreamControlAPI_L3
+PRODUCT_COPY_FILES += \
+    $(VENDOR_WV_PATH)/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    $(VENDOR_WV_PATH)/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+    $(VENDOR_WV_PATH)/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+    $(VENDOR_WV_PATH)/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    $(VENDOR_WV_PATH)/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
